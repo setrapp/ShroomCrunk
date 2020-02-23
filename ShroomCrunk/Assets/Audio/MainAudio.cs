fdc1020659b16e74ed588aa200487815a460a3cb
bearwarp <hydr01987@yahoo.com>
2020-02-22 21:31:34 -0700

    other sounds

--- ShroomCrunk/Assets/Audio/MainAudio.cs
+++ ShroomCrunk/Assets/Audio/MainAudio.cs
@@ -8,39 +8,82 @@ public class MainAudio : MonoBehaviour
     private bool audioSpeeding;
     private AudioSource poundAudio;
     public AudioClip poundClip;
+    int changeSpeed;
 
+    public AudioSource otherBackAudio;
 
     void Start()
     {
         mainAudio = GetComponent<AudioSource>();
         poundAudio = transform.Find("Pound").GetComponent<AudioSource>();
+        otherBackAudio = transform.Find("OtherBackAudio").GetComponent<AudioSource>();
     }
 
     public void AudioSpeed()
     {
         if (!audioSpeeding)
-            StartCoroutine(AudioSpeedUp());
+        {
+            if (mainAudio.volume > 0.5f)
+            {
+                 StartCoroutine(AudioSpeedUp(mainAudio, 1f));
+            }
+            else
+            {
+                StartCoroutine(AudioSpeedUp(otherBackAudio, 1.4f));
+            }
+        }
     }
-    IEnumerator AudioSpeedUp()
+    IEnumerator AudioSpeedUp(AudioSource audioJump, float mainPitchSpeed)
     {
         audioSpeeding = true;
+        changeSpeed++;
+        if (changeSpeed > 10)
+        {
+            if (otherBackAudio.volume < 1)
+            {
+                StartCoroutine(AudioVolUp(otherBackAudio));
+                StartCoroutine(AudioVolDown(mainAudio));
+
+            }
+            else
+            {
+                StartCoroutine(AudioVolDown(otherBackAudio));
+                StartCoroutine(AudioVolUp(mainAudio));
+            }
+            changeSpeed = 0;
+        }
         poundAudio.pitch = Random.Range(1.9f, 2.2f);
         poundAudio.PlayOneShot(poundClip);
-        while (mainAudio.pitch < 2.5f)
+
+        while (audioJump.pitch < 2.5f)
         {
-            mainAudio.pitch += 0.6f;
+            audioJump.pitch += 0.6f;
             yield return new WaitForSeconds(0.05f);
         }
-        while (mainAudio.pitch > 1)
+        while (audioJump.pitch > mainPitchSpeed)
         {
-            mainAudio.pitch -= 0.6f;
-            yield return null;
+            audioJump.pitch -= 0.6f;
+            yield return new WaitForSeconds(0.05f);
         }
         audioSpeeding = false;
     }
-    // Update is called once per frame
-    void Update()
+    IEnumerator AudioVolUp(AudioSource audioTurnUp)
     {
-        
+        audioTurnUp.Play();
+        while (audioTurnUp.volume < 1)
+        {
+            audioTurnUp.volume += 0.005f;
+            yield return new WaitForSeconds(0.05f);
+        }
     }
+    IEnumerator AudioVolDown(AudioSource audioTurnDown)
+    {
+        while (audioTurnDown.volume > 0)
+        {
+            audioTurnDown.volume -= 0.005f;
+            yield return new WaitForSeconds(0.05f);
+        }
+        audioTurnDown.Pause();
+    }
+
 }
