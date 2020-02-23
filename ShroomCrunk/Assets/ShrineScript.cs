dfe4446dbdb39168049c815e6d33852c26329c96
Sam Trapp <trapp.s.e@gmail.com>
2020-02-23 13:23:34 -0700

    Make sure there is a player before trying to recharge. Not sure how this wouldn't be the case

--- ShroomCrunk/Assets/ShrineScript.cs
+++ ShroomCrunk/Assets/ShrineScript.cs
@@ -218,6 +218,11 @@ public class ShrineScript : MonoBehaviour
 
 	private void RechargeSpores(bool recharging)
 	{
+		if (playerMover == null)
+		{
+			playerMover = GameObject.FindWithTag("Player").GetComponent<PlayerMover>();
+		}
+
 		if (recharging)
 		{
 			playerMover.GetComponentInChildren<WaitForSpores>().Event_DipAndWait(transform);
