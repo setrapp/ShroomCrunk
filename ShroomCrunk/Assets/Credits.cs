840c95042428f50b33f7a03ccaa615ab2f03d567
U-DESKTOP-R08VB7O\shawn <shawny@bu.edu>
2020-02-23 20:51:20 -0700

    Creditos

---
+++ ShroomCrunk/Assets/Credits.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            quitDisBitch();
+        }
+    }
+    public void quitDisBitch()
+    {
+        Application.Quit();
+    }
+}
